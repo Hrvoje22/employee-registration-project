17eb183a29794f855ee798ebf11526ad9ee2ca5d
Hrvoje <hbrkljacic@gmail.com>
2022-09-28 01:18:37 +0200

    validation

--- src/main/java/com/cydeo/model/Employee.java
+++ src/main/java/com/cydeo/model/Employee.java
@@ -3,10 +3,7 @@ package com.cydeo.model;
 import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 import java.time.LocalDate;
 
 @Data
@@ -18,9 +15,9 @@ public class Employee {
    // @NotEmpty      ---> Field shouldn't be ""
    // @NotBlank      ---> Field shouldn't be "      "
 
-    // @NotNull -> @NotNull
-    // @NotEmpty -> @NotNull + @NotEmpty
-    // @NotBlank -> @NotNull + @NotEmpty + @NotBlank
+    // @NotNull -> @NotNull                          - we can use with any kind of objects
+    // @NotEmpty -> @NotNull + @NotEmpty             - only with strings
+    // @NotBlank -> @NotNull + @NotEmpty + @NotBlank - only with strings
 
 
     @NotBlank
@@ -29,10 +26,15 @@ public class Employee {
     private String lastName;
 
     //Thymeleaf accepts yyyy-MM-dd, but LocalDate accepts mm-dd-yyyy
+    //@NotNull
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthday;
 
+    //@NotBlank
+    //@Email
     private String email;
+    //@NotBlank
+    //@Pattern(regexp = "(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{4,}")
     private String password;
     private String address;
     private String address2;
